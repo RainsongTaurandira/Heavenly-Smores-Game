2db9e65af2ce3e1c92f4d67e1069300345587e80
RainsongTaurandira <50034435+RainsongTaurandira@users.noreply.github.com>
2022-05-30 14:21:36 -0300

    added current build
    Playable build, without animations

---
+++ Assets/scripts/animate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animate : MonoBehaviour
+{
+    public Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Animator>();
+        anim.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/scripts/pickup.cs
+++ Assets/scripts/pickup.cs
@@ -7,14 +7,16 @@ public class pickup : MonoBehaviour
 {
     [SerializeField]
     private Text chocolateCounter, marshmallowCounter, crackerCounter, stickCounter;
-
     private int chocolateAmount, crackerAmount, marshmallowAmount, stickAmount;
-
+    //public AudioSource audioSource1;
+    //public AudioSource audioSource2;
+    //public AudioSource audioSource3;
+   // public AudioSource audioSource4;
     void Start()
     {
         chocolateAmount = 0;
         crackerAmount = 0;
-       marshmallowAmount = 0;
+        marshmallowAmount = 0;
         stickAmount = 0;
     }
 
@@ -31,21 +33,25 @@ public class pickup : MonoBehaviour
         if (collision.GetComponent<chocolate>())
         {
             chocolateAmount += 1;
+           // audioSource1.Play();
             Destroy(collision.gameObject);
         }
         else if (collision.GetComponent<marshmallow>())
         {
             marshmallowAmount += 1;
+          //  audioSource2.Play();
             Destroy(collision.gameObject);
         }
         else if (collision.GetComponent<cracker>())
         {
             crackerAmount += 1;
+          //  audioSource3.Play();
             Destroy(collision.gameObject);
         }
         else if (collision.GetComponent<stick>())
         {
             stickAmount += 1;
+           // audioSource4.Play();
             Destroy(collision.gameObject);
         }
     }
